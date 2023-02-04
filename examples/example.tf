decf2c83452d73ca060a7d4d59a9b4ef0000a6bf
Cole DeVries <colemandevries@gmail.com>
2023-02-04 10:33:40 -0500

    adding the example terraform

--- examples/example.tf
+++ examples/example.tf
@@ -1,13 +1,34 @@
-# update this to demonstrate usage 
-# of the module. If there are more 
-# than one examples that you want 
-# to demonstrate, create additional files in
-# this directory and name them logically. 
+module "lb_listener_rule" {
+  source = ""
 
-module "this" {
-  source = "../"
+  app     = "my-app"
+  env     = "prod"
+  program = "ccdi"
 
-  app         = "example"
-  environment = "dev"
-  program     = "example"
+  target_group_arn = aws_lb_target_group.example.arn
+  condition_host_header = [ "my-app.my-domain.gov" ]
+  condition_path_pattern = [ "/" ]
+}
+
+
+module "my_app_listener_rule" {
+  source = "AustinCloudGuru/alb/aws//modules/alb-listener-rule"
+  # You should pin the module to a specific version
+  # version                   = "x.x.x"
+  port               = "443"
+  protocol           = "HTTPS"
+  vpc_id             = "vpc-11111111111111111"
+  listener_arn       = module.https-listener.listener_arn
+  attach_certificate = true
+  certificate_arn    = module.my-app-certificate.arn
+
+  health_check = [{
+    interval          = 60
+    path              = "/"
+    timeout           = 5
+    healthy_threshold = 2
+    port              = 443
+    protocol          = "HTTPS"
+    matcher           = 200
+  }]
 }
--- main.tf
+++ main.tf
@@ -2,47 +2,41 @@ resource "aws_lb_listener_rule" "this" {
   listener_arn = var.listener_arn
   priority     = var.priority
 
-  dynamic "action" {
-    for_each = var.action_type == "forward" ? [1] : [0]
-    
+  action {
+    type             = "forward"
+    target_group_arn = var.target_group_arn
+  }
+
+  dynamic "condition" {
+    for_each = var.condition_host_header
+
     content {
-      type = var.action_type
-
-      forward {
-        stickiness {
-          enabled  = var.forward_stickiness_enabled
-          duration = var.forward_stickiness_duration
-        }
-
-        target_group {
-          arn = var.target_group_arn
-        }
+      host_header {
+        values = [var.condition_host_header]
       }
     }
-
   }
 
-  condition {
-    host_header {
-      values = var.condition_host_header
-    }
-  }
+  dynamic "condition" {
+    for_each = var.condition_path_pattern
 
-  condition {
-    path_pattern {
-      values = var.condition_path_pattern
+    content {
+      path_pattern {
+        values = [var.condition_path_pattern]
+      }
     }
   }
 }
 
+
 variable "condition_host_header" {
-  type        = list()
-  description = "matching host header values - supports '*' wildcards and is not case sensitive"
-  default     = null
+  type = list(string)
+  description = "contains a single value item which is a list of host header patterns to match"
+  default     = []
 }
 
 variable "condition_path_pattern" {
-  type        = list(any)
-  description = ""
-  default     = ""
+  type = list(string)
+  description = "Contains a single value item which is a list of path patterns to match against the request URL"
+  default     = []
 }
